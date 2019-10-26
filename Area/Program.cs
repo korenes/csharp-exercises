13596fc9cc37d1cf046f7bdfcc311154fc9ce0a9
Korene Stuart <korene17@gmail.com>
2019-10-26 14:54:04 -0400

    Added area

---
+++ Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Area
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double pi = 3.14;
+            double r, areaOfCircle;
+
+            Console.WriteLine("Enter a radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            areaOfCircle = pi * r * r;
+
+            Console.WriteLine("The area of a circle with radius "+ r +" is: " + areaOfCircle + ".");
+            Console.ReadLine();
+        }
+    }
+}
